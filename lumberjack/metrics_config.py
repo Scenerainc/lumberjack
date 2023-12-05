e54f9d75d54e6eccc472168764149ddc49368be9
Jack Corrigan <jcorrigan14@gmail.com>
2023-12-04 23:11:24 -0800

    query tf state for config values

--- lumberjack/metrics_config.py
+++ lumberjack/metrics_config.py
@@ -14,6 +14,10 @@ from dataclasses import dataclass
 from functools import cached_property
 import os
 
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobClient
+import json
+
 
 @dataclass
 class ProcessMetricsConfig:
@@ -27,6 +31,17 @@ class ProcessMetricsConfig:
         stream_name(): Azure data collection rule stream name
     """
 
+    def __init__(self):
+        storage_account_name = "stscenera002devtf"
+        container_name = "default"
+        blob_name = "layer1"
+        account_url = f"https://{storage_account_name}.blob.core.windows.net"
+        default_credential = DefaultAzureCredential()
+
+        blob_service_client = BlobServiceClient(account_url, credential=default_credential)
+        self.blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+
     @cached_property
     def environment(self) -> str:
         """Generate environment variable.
@@ -38,12 +53,17 @@ class ProcessMetricsConfig:
 
     @cached_property
     def rule_id(self) -> str:
-        """Generate Azure data collection rule ID.
+        """Generate Azure data collection rule immutable ID.
 
         Returns:
-            string : Azure data collection rule ID
+            string : Azure data collection rule immutable ID
+
+        Example:
+            "dcr-abc123"
+
         """
-        return "dcr-c03b5871ae9b4f75a3486db8796cc6e1"
+        output_key = "data_collection_rule_immutable_id"
+        return get_terraform_output_from_remote_state(self.blob_client, output_key)
 
     @cached_property
     def endpoint(self) -> str:
@@ -51,8 +71,12 @@ class ProcessMetricsConfig:
 
         Returns:
             string : Azure data collection endpoint
+
+        Example:
+            "https://<unique-dce-identifier>.<regionname>-1.ingest.monitor.azure.com"
         """
-        return "https://dce-mlops-pipeline-t02o.koreacentral-1.ingest.monitor.azure.com"
+        output_key = "data_collection_endpoint_logs_ingestion_endpoint"
+        return get_terraform_output_from_remote_state(self.blob_client, output_key)
 
     @cached_property
     def stream_name(self) -> str:
@@ -61,4 +85,22 @@ class ProcessMetricsConfig:
         Returns:
             string : Azure data collection rule stream name
         """
-        return "Custom-MLOpsPipelineLogs_CL"
+        output_key = "data_collection_rule_output_stream"
+        return get_terraform_output_from_remote_state(self.blob_client, output_key)
+
+
+# Should this take self or a blob client as the first param?
+def get_terraform_output_from_remote_state(blob_client, output_key):
+    try:
+        # Download and parse the Terraform state
+        state_content = blob_client.download_blob().readall().decode("utf-8")
+        state_data = json.loads(state_content)
+
+        # Extract the value of the specified output key
+        output_value = state_data.get('outputs', {}).get(output_key, {}).get('value')
+
+        return output_value
+    except Exception as e:
+        print(f"Error querying remote Terraform state: {e}")
+        raise
+
